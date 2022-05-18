34999b507524ebf291476c65c0db514b09f049a5
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-18 10:53:10 +0100

    Implemented ProxyContainer
    Added summaries too. Note: needs summaries to be added for everything else.

---
+++ TikTokLiveSharp/Client/Proxy/RotationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TikTokLiveSharp.Client.Proxy
+{
+    /// <summary>
+    /// Rotation settings for a proxy container.
+    /// </summary>
+    internal enum RotationSettings
+    {
+        /// <summary>
+        /// Rotate proxies consecutively, from proxy 0 -> 1 -> 2 -> ...etc.
+        /// </summary>
+        CONSECUTIVE = 1,
+        /// <summary>
+        /// Rotate proxies randomly, from proxy 0 -> 69 -> 420 -> 1 -> ...etc.
+        /// </summary>
+        RANDOM = 2,
+        /// <summary>
+        /// Don't rotate proxies at all, pin to a specific proxy index with SetPinned()
+        /// </summary>
+        PINNED = 3
+    }
+}
