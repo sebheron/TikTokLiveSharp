616a1ed175f459bfcb08e4413d2549203b7f2934
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-21 10:57:45 +0100

    Implement event handling using protobuf objects
    Ideally at some point the protobuf objects will have partial class implementations for managing the additional objects

--- TikTokLiveSharp/Client/TikTokLiveClient.cs
+++ TikTokLiveSharp/Client/TikTokLiveClient.cs
@@ -10,31 +10,28 @@ namespace TikTokLiveSharp.Client
 {
     public class TikTokLiveClient : TikTokBaseClient
     {
-        public TikTokLiveClient(string userID,
-            bool debugMode = false,
+        public TikTokLiveClient(string uniqueID,
             TimeSpan? timeout = null,
             TimeSpan? pollingInterval = null,
             Dictionary<string, object> clientParams = null,
-            Dictionary<string, string> headers = null,
             bool processInitialData = true,
             bool fetchRoomInfoOnConnect = true,
             bool enableExtendedGiftInfo = true,
-            ProxyContainer proxyContainer = null,
-            string lang = "en-US") : base(userID,
+            ProxyClientFactory proxyClientFactory = null,
+            string lang = "en-US") : base(uniqueID,
                 timeout,
                 pollingInterval,
                 clientParams,
-                headers,
                 processInitialData,
                 fetchRoomInfoOnConnect,
                 enableExtendedGiftInfo,
-                proxyContainer,
+                proxyClientFactory,
                 lang)
         { }
 
-        protected override async Task<string> connect()
+        protected override async Task<string> Connect()
         {
-            var roomID = await base.connect();
+            var roomID = await base.Connect();
             if (this.Connected && this.OnConnected != null)
             {
                 this.OnConnected.Invoke(this, new ConnectionEventArgs(true));
@@ -42,16 +39,16 @@ namespace TikTokLiveSharp.Client
             return roomID;
         }
 
-        protected override async Task disconnect()
+        protected override async Task Disconnect()
         {
-            await base.disconnect();
+            await base.Disconnect();
             if (!this.Connected && this.OnDisconnected != null)
             {
                 this.OnDisconnected.Invoke(this, new ConnectionEventArgs(false));
             }
         }
 
-        protected override void handleWebcastMessages(WebcastResponse webcastResponse)
+        protected override void HandleWebcastMessages(WebcastResponse webcastResponse)
         {
             foreach (var message in webcastResponse.Messages)
             {
@@ -68,26 +65,72 @@ namespace TikTokLiveSharp.Client
                 case nameof(WebcastChatMessage):
                     var chatMessage = ProtoBuf.Serializer.Deserialize<WebcastChatMessage>(bytes);
                     if (OnCommentRecieved != null)
-                        this.OnCommentRecieved.Invoke(this, new CommentEventArgs(chatMessage.User.uniqueId, chatMessage.Comment));
-                    break;
+                        this.OnCommentRecieved.Invoke(this, chatMessage);
+                    return;
                 case nameof(WebcastGiftMessage):
                     var giftMessage = ProtoBuf.Serializer.Deserialize<WebcastGiftMessage>(bytes);
                     if (OnGiftRecieved != null)
-                        this.OnGiftRecieved.Invoke(this, new GiftEventArgs(giftMessage.User.uniqueId, giftMessage));
-                    break;
+                        this.OnGiftRecieved.Invoke(this, giftMessage);
+                    return;
+                case nameof(WebcastLikeMessage):
+                    var likeMessage = ProtoBuf.Serializer.Deserialize<WebcastLikeMessage>(bytes);
+                    if (OnLikesRecieved != null)
+                        this.OnLikesRecieved.Invoke(this, likeMessage);
+                    return;
+                case nameof(WebcastQuestionNewMessage):
+                    var questionMessage = ProtoBuf.Serializer.Deserialize<WebcastQuestionNewMessage>(bytes);
+                    if (OnQuestionRecieved != null)
+                        this.OnQuestionRecieved.Invoke(this, questionMessage);
+                    return;
+                case nameof(WebcastRoomUserSeqMessage):
+                    var roomMessage = ProtoBuf.Serializer.Deserialize<WebcastRoomUserSeqMessage>(bytes);
+                    if (OnViewerCountUpdated != null)
+                        this.OnViewerCountUpdated.Invoke(this, roomMessage);
+                    return;
+                case nameof(WebcastSocialMessage):
+                    var eventMessage = ProtoBuf.Serializer.Deserialize<WebcastSocialMessage>(bytes);
+                    this.InvokeSpecialEvent(eventMessage);
+                    return;
+            }
+
+            if (UnhandledEvent != null)
+                this.UnhandledEvent.Invoke(this, EventArgs.Empty);
+        }
+
+        private void InvokeSpecialEvent(WebcastSocialMessage messageEvent)
+        {
+            switch (messageEvent.Event.eventDetails.displayType)
+            {
+                case "pm_mt_msg_viewer":
+                    if (OnLike != null)
+                        this.OnLike.Invoke(this, null);
+                    return;
+                case "live_room_enter_toast":
+                    if (OnJoin != null)
+                        this.OnJoin.Invoke(this, messageEvent.User);
+                    return;
+                case "pm_main_follow_message_viewer_2":
+                    if (OnFollow != null)
+                        this.OnFollow.Invoke(this, messageEvent.User);
+                    return;
+                case "pm_mt_guidance_share":
+                    if (OnShare != null)
+                        this.OnShare.Invoke(this, messageEvent.User);
+                    return;
             }
         }
 
-        public event EventHandler<CommentEventArgs> OnCommentRecieved;
+        public event EventHandler<WebcastChatMessage> OnCommentRecieved;
         public event EventHandler<ConnectionEventArgs> OnConnected;
         public event EventHandler<ConnectionEventArgs> OnDisconnected;
-        public event EventHandler<GiftEventArgs> OnGiftRecieved;
-        public event EventHandler<LikeEventArgs> OnLikesRecieved;
-        public event EventHandler<QuestionEventArgs> OnQuestionRecieved;
-        public event EventHandler<ViewerCountEventArgs> OnViewerCountUpdated;
-        public event EventHandler<UserEventArgs> OnLiveShared;
-        public event EventHandler<UserEventArgs> OnNewFollower;
-        public event EventHandler<UserEventArgs> OnJoin;
+        public event EventHandler<WebcastGiftMessage> OnGiftRecieved;
+        public event EventHandler<WebcastLikeMessage> OnLikesRecieved;
+        public event EventHandler<WebcastQuestionNewMessage> OnQuestionRecieved;
+        public event EventHandler<WebcastRoomUserSeqMessage> OnViewerCountUpdated;
+        public event EventHandler<User> OnShare;
+        public event EventHandler<User> OnFollow;
+        public event EventHandler<User> OnJoin;
+        public event EventHandler<User> OnLike;
         public event EventHandler<EventArgs> UnhandledEvent;
     }
 }
