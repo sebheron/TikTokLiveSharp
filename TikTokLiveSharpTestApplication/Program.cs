2601a21957c37feb0c00db31293d3fa4e73c2925
Seb Heron <sebheron21@hotmail.co.uk>
2022-05-21 10:59:48 +0100

    Add basic test/example project

---
+++ TikTokLiveSharpTestApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TikTokLiveSharp.Client;
+using TikTokLiveSharp.Protobuf;
+
+namespace TikTokLiveSharpTestApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a username:");
+            var client = new TikTokLiveClient(Console.ReadLine());
+            client.OnCommentRecieved += Client_OnCommentRecieved;
+            client.Run();
+        }
+
+        private static void Client_OnCommentRecieved(object sender, WebcastChatMessage e)
+        {
+            Console.WriteLine($"{e.User.uniqueId}: {e.Comment}");
+        }
+    }
+}
