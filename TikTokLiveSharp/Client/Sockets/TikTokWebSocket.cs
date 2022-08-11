e3da056463ef8bbd1165994a48b1820485a1cb21
Seb <sebheron21@hotmail.co.uk>
2022-08-11 19:19:03 +0100

    Basic web socket

---
+++ TikTokLiveSharp/Client/Sockets/TikTokWebSocket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TikTokLiveSharp.Client.Sockets
+{
+    public class TikTokWebSocket
+    {
+        private ClientWebSocket clientWebSocket;
+
+        public TikTokWebSocket(string url, CookieContainer cookieContainer)
+        {
+            this.clientWebSocket = new ClientWebSocket();
+            this.clientWebSocket.Options.AddSubProtocol("echo-protocol");
+            this.clientWebSocket.Options.Cookies = cookieContainer;
+            this.clientWebSocket.Options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+        }
+
+        public async Task Connect(string url, CancellationToken token)
+        {
+            await this.clientWebSocket.ConnectAsync(new Uri(url), token);
+        }
+
+        public bool IsConnected => this.clientWebSocket.State == WebSocketState.Open;
+    }
+}
