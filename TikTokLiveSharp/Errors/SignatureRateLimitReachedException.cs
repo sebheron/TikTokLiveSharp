b8132c8952a4e24393f178363e14bb2e37311d39
Seb <sebheron21@hotmail.co.uk>
2022-08-11 14:30:32 +0100

    Add new exceptions

---
+++ TikTokLiveSharp/Errors/SignatureRateLimitReachedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TikTokLiveSharp.Errors
+{
+    public class SignatureRateLimitReachedException : Exception
+    {
+        public SignatureRateLimitReachedException()
+        {
+        }
+
+        public SignatureRateLimitReachedException(string message) : base(message)
+        {
+        }
+
+        public SignatureRateLimitReachedException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
