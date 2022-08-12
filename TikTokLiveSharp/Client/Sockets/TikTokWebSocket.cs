3a9339980c94cf54e7cef550a6154f7871b1c5cd
Seb <sebheron21@hotmail.co.uk>
2022-08-12 04:50:00 +0100

    Improve web socket

--- TikTokLiveSharp/Client/Sockets/TikTokWebSocket.cs
+++ TikTokLiveSharp/Client/Sockets/TikTokWebSocket.cs
@@ -5,6 +5,8 @@ using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
+using TikTokLiveSharp.Client.Requests;
+using System.Linq;
 
 namespace TikTokLiveSharp.Client.Sockets
 {
@@ -12,17 +14,44 @@ namespace TikTokLiveSharp.Client.Sockets
     {
         private ClientWebSocket clientWebSocket;
 
-        public TikTokWebSocket(string url, CookieContainer cookieContainer)
+        public TikTokWebSocket(string url, TikTokCookieJar cookieContainer)
         {
             this.clientWebSocket = new ClientWebSocket();
+            
             this.clientWebSocket.Options.AddSubProtocol("echo-protocol");
-            this.clientWebSocket.Options.Cookies = cookieContainer;
             this.clientWebSocket.Options.KeepAliveInterval = TimeSpan.FromSeconds(15);
+
+            var cookieHeader = new StringBuilder();
+            foreach (var cookie in cookieContainer)
+            {
+                cookieHeader.Append(cookie);
+            }
+            this.clientWebSocket.Options.SetRequestHeader("Cookie", cookieHeader.ToString());
         }
 
-        public async Task Connect(string url, CancellationToken token)
+        public async Task Connect(string url)
         {
-            await this.clientWebSocket.ConnectAsync(new Uri(url), token);
+            await this.clientWebSocket.ConnectAsync(new Uri(url), new CancellationTokenSource(15000).Token);
+        }
+
+        public async Task WriteMessage(ArraySegment<byte> arr)
+        {
+            await this.clientWebSocket.SendAsync(arr, WebSocketMessageType.Binary, false, new CancellationTokenSource(15000).Token);
+        }
+
+        public async Task<byte[]> RecieveMessage()
+        {
+            var arr = new ArraySegment<byte>(new byte[8192]);
+            var response = await this.clientWebSocket.ReceiveAsync(arr, new CancellationTokenSource(15000).Token);
+            var i = 0;
+            for (int j = 0; j < 8192; j++)
+            {
+                if (arr.Array[j] != 0)
+                {
+                    i = j;
+                }
+            }
+            return arr.Array.Take(i).ToArray();
         }
 
         public bool IsConnected => this.clientWebSocket.State == WebSocketState.Open;
