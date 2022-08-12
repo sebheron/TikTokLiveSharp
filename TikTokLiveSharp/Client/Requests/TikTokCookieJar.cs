4e29b0ca1d424c5d2c3fffcbb1cc0e220c9e6fc3
Seb <sebheron21@hotmail.co.uk>
2022-08-12 04:49:09 +0100

    Add custom cookie jar

---
+++ TikTokLiveSharp/Client/Requests/TikTokCookieJar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TikTokLiveSharp.Client.Requests
+{
+    public class TikTokCookieJar : IEnumerable<string>
+    {
+        private IDictionary<string, string> cookies;
+
+        public TikTokCookieJar()
+        {
+            this.cookies = new Dictionary<string, string>();
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                return this.cookies[index];
+            }
+            set
+            {
+                this.cookies[index] = value;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var cookie in cookies)
+            {
+                yield return $"{cookie.Key}={cookie.Value};";
+            }
+        }
+    }
+}
